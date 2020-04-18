ec8c809e120a989c823252e0f71988587705773a
m4ik7i <m4ik7i@gmail.com>
2020-04-19 03:55:45 +0900

    feat: generate graph button

--- src/GraphOperator.jsx
+++ src/GraphOperator.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Button, Icon } from 'semantic-ui-react';
 
 export default () => {
   const dispatch = useDispatch();
 
+  const [timer, setTimer] = useState(null);
+
   return (
     <div className="GraphOperator">
+      <Button
+        primary
+        onClick={() => {
+          const timer = setInterval(() => {
+            dispatch({ type: 'nodes/add' });
+          }, 10);
+          setTimer(timer);
+        }}
+      >
+        <Icon name="play" />
+        <span>Generate Graph</span>
+      </Button>
+      <Button
+        basic
+        color="black"
+        onClick={() => {
+          if (timer) {
+            clearInterval(timer);
+            setTimer(null);
+          }
+        }}
+      >
+        <Icon name="stop" />
+        <span>Stop</span>
+      </Button>
       <Button
         primary
         onClick={() => {
